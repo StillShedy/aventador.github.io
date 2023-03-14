e4aa86c65282b847d9a445f2ca4699499b7ce6e7
denisturchak <d.turchak.it@gmail.com>
2023-03-14 16:28:04 +0200

    telegram message

--- src/AventadorWeb/AventadorWeb/Pages/Index.cshtml.cs
+++ src/AventadorWeb/AventadorWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telegram.Bot;
 
 namespace AventadorWeb.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
-    public void OnGet()
-    {
+    [BindProperty]
+    public FormModel LoginPageModel { get; set; } = new FormModel();
 
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+    {
+        if (LoginPageModel == null || string.IsNullOrEmpty(LoginPageModel.Name) || string.IsNullOrEmpty(LoginPageModel.Telephone))
+        {
+            return Page();
+        }
+
+        var client = new TelegramBotClient("6102354649:AAGLsHxfJRaNXYfbvbKeNAYQQT20zthRdZg");
+
+        try
+        {
+            await client.SendTextMessageAsync(new Telegram.Bot.Types.ChatId(-1001848878432), text: $"{LoginPageModel.Name} {LoginPageModel.Telephone}", cancellationToken: cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            try
+            {
+                await client.SendTextMessageAsync(new Telegram.Bot.Types.ChatId(-1001848878432), text: ex.Message, cancellationToken: cancellationToken);
+            }
+            catch { }
+        }
+
+        return Page();
     }
+}
+
+public class FormModel
+{
+    public string Name { get; set; }
+    public string Telephone { get; set; }
 }
 
